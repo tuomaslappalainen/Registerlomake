ed58bd740b734453041870ff695dae65e4e79bb4
Tuomas Lappalainen <tuomas.lappalainen@esedulainen.fi>
2023-09-08 12:45:20 +0300

    Felix fix

--- main.js
+++ main.js
@@ -1,14 +1,52 @@
-function formValidation()
-{
-    var uid = document.registration.userid;
-    var salasanaid = document.registration.salasanaid;
-    var knimi = document.registration.knimi;
-    var osoite = document.registration.osoite;
-    var maa = document.registration.maa;
-    var postiosoite = document.registration.postiosoite;
-    var sposti = document.registration.sposti;
-    var mies = document.registration.mies;
-    var nainen = document.registration.nainen; if(userid_validation(uid,5,12))
+
+
+
+function formValidation(){
+
+
+    var x = document.forms["registration"]["osoite"].value;
+    var z = document.forms["registration"]["kayttajaid"].value;
+    var c = document.forms["registration"]["salasanaid"].value;
+    var v = document.forms["registration"]["kayttajanimi"].value;
+    var b = document.forms["registration"]["zip"].value;
+    var n = document.forms["registration"]["email"].value;
+   
+    if (x == "") {
+    alert("Osoite puuttuu");
+    return false;
+    }
+
+     if (z == "") {
+    alert("Käyttäjä ID puuttuu");
+    return false;
+
+     }
+
+     if (c == "") {
+    alert("Salasana puuttuu");
+    return false;
+
+     }
+
+     if (v == "") {
+    alert("Nimi puuttuu");
+    return false;
+
+     }
+
+     if (b == "") {
+    alert("Postinumero puuttuu");
+    return false;
+
+     }
+
+     if (n == "") {
+    alert("Sähköposti puuttuu");
+    return false;
+
+     }
+  
+    if(userid_validation(uid,5,12))
     {
         if(salasanaid_validation(salasanaid,7,12))
         {
@@ -24,7 +62,7 @@ function formValidation()
                             {
                                 if(validspuoli(mies,nainen))
                                 {
-
+                                  
                                 }
                             }
                         }
@@ -32,7 +70,7 @@ function formValidation()
                 }
             }
         }
+
     }
-return false;
 
 }
